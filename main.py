a3a7cc7b7c6c42fc33e099f52551d0ecd4d79df4
riiswa <wradji64@gmail.com>
2020-11-23 15:29:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from tag_detector import UserTagChecker
+import instaloader
+import logging
+import getpass
+from instapy import InstaPy
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    loader = instaloader.Instaloader()
+
+    user = input("Your Instagram Username: ")
+    passwd = getpass.getpass("Password for " + user + ": ")
+    session = InstaPy(username=user, password=passwd, headless_browser=True)
+    loader.login(user, passwd)
+
+    user = input("Enter the instagram username of the account to be scanned: ")
+    logging.info("Start of process...")
+    ihc = UserTagChecker(user, loader, session)
+    logging.info(f"Searching hashtags for user {user}...")
+    ihc.get_user_hashtags()
+    logging.info("End of process.")
+    logging.info(f"Found {len(ihc.tags)} tags.")
+    banned_tag = ihc.get_banned_hashtags()
+    logging.info(f"Found {len(banned_tag)} banned tag(s).")
+    if len(banned_tag):
+        logging.info(f"Banned hashtags are: {', '.join([f'#{tag}' for tag in banned_tag])}")
+
+
---
+++ tag_detector.py
@@ -0,0 +1,84 @@
+from itertools import chain
+import json
+from sys import exit
+import logging
+from contextlib import contextmanager
+
+from instaloader import Profile
+from tqdm import tqdm
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+@contextmanager
+def custom_smart_run(session, threaded=False):
+    try:
+        session.login()
+        yield
+    except KeyboardInterrupt:
+        exit("You have exited successfully.")
+    finally:
+        session.end(threaded_session=threaded)
+
+
+def hashtag_is_valid(tag, browser, delay=5):
+    """
+    Check if a given hashtag is banned by Instagram
+
+    :param delay: Maximum time to search for information on a page
+    :param browser: A Selenium Driver
+    :param tag: The hashtag to check
+    :return: True if the hashtag is valid, else False
+    """
+    try:
+        url = f"https://www.instagram.com/explore/tags/{tag}/?__a=1"
+        browser.get(url)
+        WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'json')))
+        content = browser.find_element_by_id('json').text
+        parsed_json = json.loads(content)
+        return parsed_json['graphql']['hashtag']['allow_following']
+    except TimeoutException:
+        logging.warning(
+            f'Error while checking the tag #{tag}. Loading took too much time, Please check your internet connexion.')
+        return True
+    except KeyError:
+        return False
+
+class UserTagChecker:
+    """
+    Utils for hashtags checker
+    """
+
+    def __init__(self, user, insta_loader, session):
+        """
+        :param user: A Instagram username
+        :param insta_loader: An Instance InstaLoader
+        """
+        self.user = user
+        self.tags = set()
+        self.insta_loader = insta_loader
+        self.session = session
+
+    def get_user_hashtags(self):
+        """
+        Defined the list of user hashtags from all user posts
+        """
+        self.tags = set(chain(
+            *[post.caption_hashtags for post in
+              Profile.from_username(self.insta_loader.context, self.user).get_posts()]))
+
+    def get_banned_hashtags(self):
+        """
+        Search banned hashtags from the list of user hashtags
+
+        :return: The list of banned hashtags
+        """
+        banned_hashtag_list = []
+        with custom_smart_run(self.session):
+            for tag in tqdm(self.tags):
+                if not hashtag_is_valid(tag, self.session.browser):
+                    banned_hashtag_list.append(tag)
+        return banned_hashtag_list
